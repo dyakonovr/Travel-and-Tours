7f1625ffdd473d38a98ea803f866c016729dde13
RuslanDyakonov <dyakonov.rr@bk.ru>
2020-12-06 14:04:41 +0400

    ignore scss, upgrade js

--- Work_package/js/script.js
+++ Work_package/js/script.js
@@ -3,12 +3,14 @@ $(document).ready(function () {
   //* vars
   var nowDate = new Date(),
     // date
+    days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"],
+    day = days[nowDate.getDay()],
     month = nowDate.getMonth() + 1, // 0 соответствует январю, 1 — февралю и так далее.
-    day = nowDate.getDate(),
+    dateNow = nowDate.getDate(),
     year = nowDate.getFullYear(),
-    today = day + '.' + month + '.' + year,
-    tomorrow = (day + 1) + '.' + month + '.' + year,
-    afterTomorrow = (day + 2) + '.' + month + '.' + year,
+    today = days[nowDate.getDay()] + ',' + ' ' + dateNow + '.' + month + '.' + year,
+    tomorrow = days[nowDate.getDay() + 1] + ',' + ' ' + (dateNow + 1) + '.' + month + '.' + year,
+    afterTomorrow = days[nowDate.getDay() + 2] + ',' + ' ' + (dateNow + 2) + '.' + month + '.' + year,
     // modal
     MD = $(".modal"), /* MD - modal window */
     MDcont = $(".modal-container"),
@@ -21,39 +23,46 @@ $(document).ready(function () {
 
   //? code
 
-  $('option[id="today"]').text(today);
-  $('option[id="tomorrow"]').text(tomorrow);
-  $('option[id="afterTomorrow"]').text(afterTomorrow);
+  console.log(day);
+
+  function setDate() {
+    $('option[id="today"]').text(today);
+    $('option[id="tomorrow"]').text(tomorrow);
+    $('option[id="afterTomorrow"]').text(afterTomorrow);
+
+    if (day < 10) {
+      var day = '0' + day
+    }
+  }
+
+
 
   //* == modal open ==
 
-
-  MDOpenLog.on('click', function () {
-    MD.fadeIn('400');
-  });
-
-  MDOpenReg.on('click', function () {
-    MD.fadeIn('400');
-    RepeatPW.removeAttr("hidden");
-  });
-
-  //* == /modal open ==
-
-  //? == modal close ==
-
-  MDClose.on('click', function () {
-    MD.fadeOut('400', function () {
-      if (RepeatPW.attr('hidden', true)) {
-      } else {
-        RepeatPW.attr("hidden", false);
-      }
+  function openModal() {
+    MDOpenLog.on('click', function () {
+      MD.fadeIn('400');
     });
-  });
 
-  //? == /modal close ==
+    MDOpenReg.on('click', function () {
+      MD.fadeIn('400');
+      RepeatPW.removeAttr("hidden");
+    });
+  }
 
+  function closeModal() {
+    MDClose.on('click', function () {
+      MD.fadeOut('400', function () {
+        if (RepeatPW.attr('hidden', true)) {
+        } else {
+          RepeatPW.attr("hidden", false);
+        }
+      });
+    });
+  }
 
-
-
+  setDate();
+  openModal();
+  closeModal();
 });
 
